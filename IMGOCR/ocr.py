5a04bab7a8b0ea6e7166281573122a260f7a2939
Chi-Wen "Toby" Teng <52705034+toby0622@users.noreply.github.com>
2023-08-23 21:21:12 +0800

    Update ocr.py

--- IMGOCR/ocr.py
+++ IMGOCR/ocr.py
@@ -7,12 +7,12 @@ import datetime
 def image_ocr_match(image_path, counter_number):
     process_start = datetime.datetime.now()  # process starting time
 
-    ocr_model = PaddleOCR(lang="ch", use_gpu=True, enable_mkldnn=True)
+    # ocr_model = PaddleOCR(lang="ch", use_gpu=True, enable_mkldnn=True)
 
-    # ocr_model = PaddleOCR(use_angle_cls=True, lang="ch", use_gpu=True, enable_mkldnn=True,
-    #                 rec_model_dir='models/ch_PP-OCRv3_rec_infer.tar',
-    #                 cls_model_dir='models/ch_ppocr_mobile_v2.0_cls_infer.tar',
-    #                 det_model_dir='models/ch_PP-OCRv3_det_infer.tar')
+    ocr_model = PaddleOCR(use_angle_cls=True, lang="ch", use_gpu=True, enable_mkldnn=True,
+                    rec_model_dir='models/ch_PP-OCRv3_rec_infer.tar',
+                    cls_model_dir='models/ch_ppocr_mobile_v2.0_cls_infer.tar',
+                    det_model_dir='models/ch_PP-OCRv3_det_infer.tar')
 
     # OpenCV threshold process
     image = cv2.imread(image_path, cv2.IMREAD_COLOR)
